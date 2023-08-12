3b8d47fefe3555288c8aa1318a312bd6dc982da1
Bakar002 <abubakaramjad78600@gmail.com>
2023-08-12 06:30:34 +0500

    basic

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import '@/styles/globals.css'
+
+
+export default function App({ Component, pageProps }) {
+
+  return (
+  
+  <>
+  <Header></Header>
+  <div className="container mx-auto min-h-screen">
+  <Component {...pageProps} /></div>
+  <Footer></Footer>
+  </>
+  )
+}
